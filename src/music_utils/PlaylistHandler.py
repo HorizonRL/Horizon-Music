2724b768d7764580f4ed4aaf5fa41da732330f1f
Ofek Harel <ofekizh@gmail.com>
2020-05-08 00:22:57 +0300

    FUCK U
    Signed-off-by: Ofek Harel <ofekizh@gmail.com>

--- src/music_utils/PlaylistHandler.py
+++ src/music_utils/PlaylistHandler.py
@@ -9,10 +9,20 @@ def create_all_music_playlist():
         if file.endswith(".mp3"):
             my_playlist.songs.append(Song(os.path.join(PATH_TO_MUSIC_LIB, file)))
 
-    print(my_playlist.string())
+    return my_playlist
+
+
+def create_downloaded_music_playlist():
+    my_playlist = Playlist(name="My Songs")
+    PATH_TO_MUSIC_LIB = os.path.join(os.getcwd(), r'music_utils\music_lib')
+    for file in os.listdir(PATH_TO_MUSIC_LIB):
+        if file.endswith(".mp3"):
+            my_playlist.songs.append(Song(os.path.join(PATH_TO_MUSIC_LIB, file)))
+
     return my_playlist
 
 
 class PlaylistHandler:
     def __init__(self):
         self.all_music = create_all_music_playlist()
+        self.my_music = create_downloaded_music_playlist()
--- src/ui/UIHandler.py
+++ src/ui/UIHandler.py
@@ -1,11 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
-from kivy.uix.button import ButtonBehavior
+from kivy.uix.button import ButtonBehavior, Button
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
+from kivy.uix.scrollview import ScrollView
 
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
@@ -49,7 +52,6 @@ class AllSongsScreen(Screen):
 
 '''
     Widgets & Utils
-
 '''
 
 
